783ac996c518ef87954d9de1c0b9aedb050a4b11
bude <xubobox@gmail.com>
2018-10-21 15:30:48 +0800

    add json rpc example

---
+++ JsonRPC/jsonrpc_server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"os"
+)
+
+// 算术运算结构体
+type Arith struct {
+}
+
+// 算术运算请求结构体
+type ArithRequest struct {
+	A int
+	B int
+}
+
+// 算术运算响应结构体
+type ArithResponse struct {
+	Pro int
+	Quo int
+	Rem int
+}
+
+// 乘法运算方法
+func (this *Arith) Multiply(req ArithRequest, res *ArithResponse) error {
+	res.Pro = req.A * req.B
+	return nil
+}
+
+// 除法运算方法
+func (this *Arith) Divide(req ArithRequest, res *ArithResponse) error {
+	if req.B == 0 {
+		return errors.New("divide by zero")
+	}
+	res.Quo = req.A / req.B
+	res.Rem = req.A % req.B
+	return nil
+}
+
+func main() {
+	rpc.Register(new(Arith)) // 注册rpc服务
+
+	lis, err := net.Listen("tcp", "127.0.0.1:8096")
+	if err != nil {
+		log.Fatalln("fatal error:", err)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s", "start connection")
+
+	for {
+		conn, err := lis.Accept() // 接收客户端连接请求
+		if err != nil {
+			continue
+		}
+
+		go func(conn net.Conn) {
+			// 并发处理客户端请求
+			fmt.Fprintf(os.Stdout, "%s", "new client in coming\n")
+			jsonrpc.ServeConn(conn)
+		}(conn)
+	}
+}
